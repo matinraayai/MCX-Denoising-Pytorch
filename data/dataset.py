2ce848f8e314387a62b0a6e0538ef826f38470f2
matinraayai <raayaiardani.m@northeastern.edu>
2020-11-19 17:40:13 -0500

    Somewhat working pipeline.

--- data/dataset.py
+++ data/dataset.py
@@ -2,21 +2,22 @@ from abc import ABC
 
 import tensorflow as tf
 import scipy.io as spio
+import numpy as np
 
 
 class OsaDataset(tf.data.Dataset, ABC):
     @staticmethod
     def _generator(root: str, input_file_name: str, label_file_name: str, num_samples: int):
-        for i in range(num_samples):
-            x = spio.loadmat(root + input_file_name.format(i), squeeze_me=True)['data']
-            y = spio.loadmat(root + label_file_name.format(i), squeeze_me=True)['data']
+        for i in range(1, num_samples + 1):
+            x = spio.loadmat(root + input_file_name % i, squeeze_me=True)['data']
+            y = spio.loadmat(root + label_file_name % i, squeeze_me=True)['data']
             for z in range(x.shape[2]):
-                yield x[z], y[z]
+                yield np.expand_dims(np.array([x[z], y[z]]), axis=-1)
 
     def __new__(cls, root: str, input_file_name: str, label_file_name: str, num_samples: int):
         return tf.data.Dataset.from_generator(
             cls._generator,
             output_types=tf.dtypes.float32,
-            output_shapes=(2, 100, 100),
+            output_shapes=(2, 100, 100, 1),
             args=(root, input_file_name, label_file_name, num_samples)
         )
--- train.py
+++ train.py
@@ -6,11 +6,11 @@ import tqdm
 
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--batch-size', dest='batch_size', type=int, default=64, help='batch size used for training')
+parser.add_argument('--batch-size', dest='batch_size', type=int, default=1, help='batch size used for training')
 parser.add_argument('--num-epochs', dest='num_epochs', type=int, default=300, help='number of training epochs')
 parser.add_argument('--learning-rate', dest='lr', type=float, default=1e-4, help='learning rate of the optimizer')
 parser.add_argument('--save_dir', dest='save_dir', default='./patches', help='dir of patches')
-parser.add_argument('--data-root', dest='data_root', default='./osa', help='path to where the data is located')
+parser.add_argument('--data-root', dest='data_root', default='mcxlab/osa/', help='path to where the data is located')
 parser.add_argument('--input-file-name', dest='input_file_name', default='1e+05/%d.mat')
 parser.add_argument('--input-label-name', dest='input_label_name', default='1e+07/%d.mat')
 parser.add_argument('--dataset-length', dest='dataset_length', type=int, default=100)
@@ -19,18 +19,19 @@ args = parser.parse_args()
 
 def main(kwargs):
     optimizer = tf.keras.optimizers.Adam(learning_rate=kwargs.lr)
-    loss = tf.nn.l2_loss()
-    train_dataset = OsaDataset(kwargs.root, kwargs.input_file_name,
+    loss = tf.keras.losses.MeanSquaredError()
+    train_dataset = OsaDataset(kwargs.data_root, kwargs.input_file_name,
                                kwargs.input_label_name, kwargs.dataset_length).batch(args.batch_size)
     model = CascadedDnCNNWithUNet(num_dcnn=1)
 
     for epoch_num in range(kwargs.num_epochs):
-        iterator = tqdm.tqdm(enumerate(train_dataset))
-        iterator.set_description("Epoch #{}".format(epoch_num))
+        iterator = tqdm.tqdm(train_dataset.enumerate())
+        iterator.set_description(f"Epoch #{epoch_num}:")
 
     # Iterate over the batches of the dataset.
-        for step, (x_batch_train, y_batch_train) in iterator:
-
+        for step, thing in iterator:
+            x_batch_train = thing[:, 0, :, :, :]
+            y_batch_train = thing[:, 1, :, :, :]
             # Open a GradientTape to record the operations run
             # during the forward pass, which enables auto-differentiation.
             with tf.GradientTape() as tape:
@@ -53,9 +54,9 @@ def main(kwargs):
                 optimizer.apply_gradients(zip(grads, model.trainable_weights))
 
         # Log every 200 batches.
-        if step % 200 == 0:
-            print("Training loss (for one batch) at step %d: %.4f" % (step, float(loss_value)))
-            print("Seen so far: %s samples" % ((step + 1) * 64))
+            if step % 5 == 0:
+                print("Training loss (for one batch) at step %d: %.4f" % (step, float(loss_value)))
+                print("Seen so far: %s samples" % ((step + 1) * 64))
 
 
 if __name__ == '__main__':
