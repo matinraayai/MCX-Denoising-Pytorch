330293ff9ea4bc19336b90208ffd80d21b8be934
matinraayai <raayaiardani.m@northeastern.edu>
2020-11-18 21:32:14 -0500

    A revised data-generation script.

--- data/genImage_osa_1e5_to_1e8.m
+++ data/genImage_osa_1e5_to_1e8.m
@@ -1,41 +1,46 @@
-%% generating data sets used in OSA
+% Generates data sets used in OSA
+% Input arguments:
+TOP_FOLDER_NAME = 'osa_data';
+MCXLAB_PATH = "./mcx/mcxlab";
+NUM_SIMULATIONS = 1000;
+X = 1000;
+Y = 1000;
+Z = 1000;
+PHOTON_COUNTS = [1e5, 1e6, 1e7, 1e8, 1e9];
+
 
 clear all
-
 % set path for mcxlab
-addpath(genpath('./mcx/mcxlab'))
+addpath(genpath(MCXLAB_PATH))
 
-% Top-level Dir
-topFolderName='osa_data';
-if ~exist('osa_data', 'dir')  mkdir(topFolderName); end
+% Create dataset folder if already not present
+if ~exist(TOP_FOLDER_NAME, 'dir')
+    mkdir(TOP_FOLDER_NAME);
+end
 
-N = 100; % run N times simulation for each test, 
-x = 100;
-y = 100;
-z = 100;
-
-pho_cnt = [1e5, 1e6, 1e7, 1e8];  % use 10 x 1e8 for 1e9 (as the ground truth)
-volume = uint8(ones(x,y,z));
+volume = uint8(ones(X,Y,Z));
 
 
-for k=1:length(pho_cnt)
-	% Generate new random seed for Monte Carlo simulation
-	rand_seed = randi([1 2^31-1], 1, N);
-	
-	if (length(unique(rand_seed)) < length(rand_seed)) ~= 0
-		error('There are repeated random seeds!')
-	end
+for phtn_cnt = 1:length(PHOTON_COUNTS)
+    % Generate unique random seeds for Monte Carlo simulation
+    are_seed_unique = 1;
+	while are_seed_unique ~= 0
+	    rand_seed = randi([1 2^31 - 1], 1, NUM_SIMULATIONS);
+	    are_seed_unique = length(unique(rand_seed)) < length(rand_seed)
 
-	dir_phn = sprintf('./%s/%1.0e', topFolderName, pho_cnt(k));
-  if ~exist(dir_phn, 'dir')  mkdir(dir_phn); end
+	dir_phn = sprintf('./%s/%1.0e', topFolderName, PHOTON_COUNTS(phtn_cnt));
+    if ~exist(dir_phn, 'dir')
+        mkdir(dir_phn);
+    end
 
-	for tid =1:N
-		dir_phn_test = sprintf('%s/%d', dir_phn, tid);
-        if ~exist(dir_phn_test, 'dir')  mkdir(dir_phn_test); end
-
+	for sim_id = 1:NUM_SIMULATIONS
+		dir_phn_test = sprintf('%s/%d', dir_phn, sim_id);
+        if ~exist(dir_phn_test, 'dir')
+            mkdir(dir_phn_test);
+        end
 
 		clear cfg
-		cfg.nphoton=pho_cnt(k);
+		cfg.nphoton=PHOTON_COUNTS(phtn_cnt);
 		cfg.vol= volume;
 		cfg.srcpos=[50 50 1];
 		cfg.srcdir=[0 0 1];
@@ -46,7 +51,7 @@ for k=1:length(pho_cnt)
 		cfg.tstart=0;
 		cfg.tend=5e-8;
 		cfg.tstep=5e-8;
-		cfg.seed = rand_seed(tid); % each random seed will have different pattern 
+		cfg.seed = rand_seed(sim_id); % each random seed will have different pattern
 
 		% calculate the flux distribution with the given config
 		[flux,detpos]=mcxlab(cfg);
@@ -54,10 +59,10 @@ for k=1:length(pho_cnt)
 		image3D=flux.data;
 
 		%%% export each image in 3D volume
-		for imageID=1:y
-			fname = sprintf('%s/osa_phn%1.0e_test%d_img%d.mat', dir_phn_test, pho_cnt(k), tid, imageID);
+		for imageID=1:Z
+			fname = sprintf('%s/osa_phn%1.0e_test%d_img%d.mat', dir_phn_test, PHOTON_COUNTS(phtn_cnt), sim_id, imageID);
 			fprintf('Generating %s\n',fname);
-			currentImage = squeeze(image3D(:,imageID,:));
+			currentImage = squeeze(image3D(:,:,imageID));
 			feval('save', fname, 'currentImage');
 		end
 	end
