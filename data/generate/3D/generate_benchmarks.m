9d08916e5e117fe049226bb641e7fead4551ed15
matinraayai <raayaiardani.m@northeastern.edu>
2021-03-26 01:55:19 -0400

    Bug fix.

--- data/generate/3D/generate_benchmarks.m
+++ data/generate/3D/generate_benchmarks.m
@@ -5,7 +5,8 @@ GPU_IDs = '11';
 DATA_DIMs = [100 100 100];
 % Top level directory
 TOP_FOLDER_NAME = './absorb';
-TYPE = 'absorb'
+TYPE = 'absorb';
+addpath('../mcxlab');
 
 file_id = 0;
 for i = 1 : N
