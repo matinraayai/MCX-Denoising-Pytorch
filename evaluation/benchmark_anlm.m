b13c7b91174f30928c28e1d839ea59e68780fae4
matinraayai <raayaiardani.m@northeastern.edu>
2021-03-28 02:08:50 -0400

    Working model inference and anlm benchmarks.

--- evaluation/benchmark_anlm.m
+++ evaluation/benchmark_anlm.m
@@ -1,13 +1,17 @@
 
-function benchmark_anlm(input_dir, output_dir, v, f1, f2)
+function benchmark_anlm(input_dir, output_dir, v, f1, f2, rician, gpuid, bw)
     %
     % Applies the MCX filter on the benchmark datasets and saves them to an output file.
     % input_path and output_path should be in C-formatted strings to point to correct files
     %
-    if nargin == 3
+    if nargin == 2
         v = 3;
         f1 = 1;
         f2 = 2;
+        rician = 1;
+        gpuid = 0;
+        bw = 8;
+
     end
     % Add MCX filter mex file to path
     addpath('../octave');
@@ -19,9 +23,11 @@ function benchmark_anlm(input_dir, output_dir, v, f1, f2)
     end
     % Apply the filter to each input file and save it in the output
     for i = 1 : length(path_list)
+
         current_file_name = path_list{i};
-        input_file_path = strcat(input_dir, current_file_name);
-        output_file_path = strcat(output_dir, current_file_name);
+        fprintf("Processing file %s\n", current_file_name);
+        input_file_path = strcat(input_dir, '/', current_file_name);
+        output_file_path = strcat(output_dir, '/', current_file_name);
         % Each input file will have a simulation for a particular number of photons. We will have to identify and
         % preserve each labe
         labels = {whos('-file', input_file_path).name};
@@ -30,8 +36,9 @@ function benchmark_anlm(input_dir, output_dir, v, f1, f2)
         % Apply the filter on each simulation
         for j = 1 : length(labels)
             current_label = labels{j};
-            current_simulation = input_file{current_label};
-            current_output = mcxfilter(current_simulation, v, f1, f2);
+            fprintf("Applying filter to %s\n", current_label);
+            current_simulation = getfield(input_file, current_label);
+            current_output = mcxfilter(current_simulation, v, f1, f2, rician, gpuid, bw);
             % A hack I picked up to assign a variable named current_label to the current_output
             eval(sprintf("%s = current_output;", current_label));
             % Save the results to the output directory
--- octave/make_path_list.m
+++ octave/make_path_list.m
@@ -2,4 +2,4 @@ function path_list = make_path_list(path)
     % Creates a list of files located in the path as a CS list
     listing = dir(path);
     path_list = {listing.name};
-    path_list = {path_list{3:end}}
+    path_list = {path_list{3:end}};
