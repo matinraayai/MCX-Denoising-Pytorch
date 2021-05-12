9353d31fe3e535409fafe335f0c989617af37a7b
matinraayai <raayaiardani.m@northeastern.edu>
2021-05-12 11:26:47 -0400

    Some bug fix + compatibility with Octave.

--- evaluation/benchmark_anlm.m
+++ evaluation/benchmark_anlm.m
@@ -1,20 +1,20 @@
-
 function benchmark_anlm(input_dir, output_dir, v, f1, f2, rician, gpuid, bw)
     %
     % Applies the MCX filter on the benchmark datasets and saves them to an output file.
     % input_path and output_path should be in C-formatted strings to point to correct files
     %
+    % Default parameters used in the GPU-ANLM paper
     if nargin == 2
         v = 3;
         f1 = 1;
         f2 = 2;
-        rician = 1;
+        rician = 0;
         gpuid = 0;
         bw = 8;
 
     end
     % Add MCX filter mex file to path
-    addpath('../octave');
+    addpath('../matlab');
     % Get a cell array of all the files located in the input_dir
     path_list = make_path_list(input_dir);
     % Create the output directory if it doesn't exist already
@@ -39,13 +39,12 @@ function benchmark_anlm(input_dir, output_dir, v, f1, f2, rician, gpuid, bw)
             fprintf("Applying filter to %s\n", current_label);
             current_simulation = getfield(input_file, current_label);
             current_output = mcxfilter(current_simulation, v, f1, f2, rician, gpuid, bw);
-            % A hack I picked up to assign a variable named current_label to the current_output
             eval(sprintf("%s = current_output;", current_label));
             % Save the results to the output directory
             if j == 1
-                save(output_file_path, current_label);
+                save(output_file_path, '-mat-binary', current_label);
             else
-                save(output_file_path, current_label, '-append');
+                save(output_file_path, current_label, '-append', '-mat-binary');
             end
         end
     end
