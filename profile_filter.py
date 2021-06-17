fd01721d92dddc8248a13c7ec8409381d5fe6f8f
matinraayai <raayaiardani.m@northeastern.edu>
2021-06-17 13:19:52 -0400

    Minor change to the profiler. Removed loading checkpoint stage as it was irrelevant, added 3D model profiling.

--- profile_filter.py
+++ profile_filter.py
@@ -1,21 +1,36 @@
 from model.builder import get_model
 import argparse
 import tqdm
-from config import get_cfg_defaults
+from config import get_default_training_cfg
 import torch
 import torch.autograd.profiler as profiler
-from time import time
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Model Performance Profiler")
-    parser.add_argument('--config-file', type=str, help='configuration file (yaml)')
-    parser.add_argument('--checkpoint', type=str, default=None, help='path to load the checkpoint')
+    parser = argparse.ArgumentParser(description="CNN Model Performance Profiler")
+    parser.add_argument('--config-file', type=str, help='configuration file (yaml). It will only use the model'
+                                                        'part of the training configuration')
     parser.add_argument('--num-iterations', type=int, default=100, help='Number of iterations for profiling')
     return parser.parse_args()
 
+
+def get_batch_input_shape(cfg):
+    """
+    TODO: Move this to config.py.
+    :return: The input data batch shape for the CNN
+    """
+    # Select 2D or 3D model profiling based on the architecture
+    model_architecture = cfg.model.architecture
+    dim = 3 if getattr(cfg.model, model_architecture).do_3d else 2
+    if dim == 3:
+        return (1, 1, 64, 64, 64) if cfg.dataset.crop_size is None else (1, 1, 1, *cfg.dataset.crop_size)
+    elif dim == 2:
+        return (1, 1, 100, 100) if cfg.dataset.crop_size is None else (1, 1, *cfg.dataset.crop_size)
+    else:
+        raise ValueError(f"Model dim {dim} is invalid.")
+
+
 def main():
-    r"""Main function."""
     # arguments
     args = get_args()
 
@@ -23,18 +38,15 @@ def main():
     print(args)
 
     # configurations
-    cfg = get_cfg_defaults()
+    cfg = get_default_training_cfg()
     cfg.update()
 
     cfg.merge_from_file(args.config_file)
 
-    # Load model and its checkpoint
     model = get_model(**cfg.model).cuda()
-    state_dict = torch.load(args.checkpoint)
-    model.load_state_dict(state_dict.state_dict())
     model.train(False)
 
-    data_dims = (1, 1, 100, 100) if cfg.dataset.crop_size is None else (1, 1, *cfg.dataset.crop_size)
+    data_dims = get_batch_input_shape(cfg)
     x_input = [torch.rand(data_dims) for _ in range(args.num_iterations)]
     with profiler.profile(record_shapes=True) as prof:
         for x in tqdm.tqdm(x_input):
