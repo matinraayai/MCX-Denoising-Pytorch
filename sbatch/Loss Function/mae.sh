687642dd38f022742871681ce39810f2d3f001b8
matinraayai <raayaiardani.m@northeastern.edu>
2021-07-22 19:19:46 -0400

    Added activation function and loss function ablation study configurations.

---
+++ sbatch/Loss Function/mae.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1                # Number of nodes
+#SBATCH -c 64
+#SBATCH -p ce-mri          # Partition to submit to
+#SBATCH --gres=gpu:a100:1   # Number of GPUs-per-node
+#SBATCH --mem=16000         # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o sbatch_outputs/loss-function/mae_%j.out  # %j inserts jobid
+#SBATCH -e sbatch_outputs/loss-function/mae_%j.err  # %j inserts jobid
+source ~/modules/pytorch/latest
+PL_TORCH_DISTRIBUTED_BACKEND=nccl python train-lightning.py --config-file configs/Loss\ Function/3D-x1e5-MAE.yaml
