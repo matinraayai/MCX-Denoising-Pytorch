6068b8098cb09504fef72cceb869beb1e65b04ae
matinraayai <raayaiardani.m@northeastern.edu>
2021-09-21 13:49:00 -0400

    Updated model inference

--- model_inference.py
+++ model_inference.py
@@ -5,10 +5,10 @@ import os
 import torch
 from torch.utils.data import DataLoader
 from data.dataset import OsaDataset
-from model.builder import load_model_from_checkpoint
+from model.builder import create_model_from_lightning_checkpoint
 import argparse
 import tqdm
-from config import get_cfg_defaults
+from config import get_default_inference_cfg
 from time import time
 import scipy.io as scio
 
@@ -19,26 +19,6 @@ def get_args():
     return parser.parse_args()
 
 
-def infer(model, x, denoise_3d_with_2d):
-    if denoise_3d_with_2d:
-        y = torch.zeros_like(x)
-        # Infer on x-axis
-        for i in range(x.shape[2]):
-            x_2d = x[:, :, i, :, :].squeeze(2)
-            y[:, :, i, :, :] = model(x_2d)
-        # Infer on y-axis
-        for i in range(x.shape[3]):
-            x_2d = x[:, :, :, i, :].squeeze(3)
-            y[:, :, :, i, :] = model(x_2d)
-        # Infer on z-axis
-        for i in range(x.shape[4]):
-            x_2d = x[:, :, :, :, i]
-            y[:, :, :, :, i] = model(x_2d)
-        return y
-    else:
-        return model(x)
-
-
 def main():
     args = get_args()
 
@@ -46,7 +26,7 @@ def main():
     print(args)
 
     # configurations
-    cfg = get_cfg_defaults()
+    cfg = get_default_inference_cfg()
     cfg.update()
 
     cfg.merge_from_file(args.config_file)
@@ -55,18 +35,18 @@ def main():
     print("Configuration details:")
     print(cfg)
 
-    # Load model and its checkpoint
-    model = load_model_from_checkpoint(cfg.inference.checkpoint_dir, **cfg.model)
+    model = create_model_from_lightning_checkpoint(cfg.model.checkpoint, **cfg.model)
+
     test_dataset = OsaDataset(cfg.dataset.test_path, cfg.dataset.input_labels, cfg.dataset.output_label,
-                              False, cfg.dataset.crop_size)
+                              False)
     test_dataloader = DataLoader(test_dataset, 1, shuffle=False, num_workers=cfg.dataset.dataloader_workers,
                                  pin_memory=True)
 
     model.train(False)
 
     iterator_test = tqdm.tqdm(test_dataloader)
-    iterator_test.set_description(f"Benchmark Progress")
-    test_output_dir = os.path.join(cfg.inference.output_dir)
+    iterator_test.set_description("Benchmark Progress")
+    test_output_dir = os.path.join(cfg.output_dir)
     os.makedirs(test_output_dir, exist_ok=True)
 
     for i, (x_tests, y_test) in enumerate(iterator_test):
@@ -75,12 +55,15 @@ def main():
         with torch.no_grad():
             for label, x_test in x_tests.items():
                 start = time()
-                prediction = infer(model, x_test, cfg.inference.denoise_3d_with_2d)
+                prediction = model(x_test)[test_dataset.unpaded_volume_slice]
                 prediction = (torch.exp(prediction) - 1).squeeze().cpu().numpy()
                 end = time()
-                iterator_test.set_postfix({"Inf. time": "{:.5f}".format(end - start)})
+                iterator_test.set_postfix({"Inf. time": "{:.5f}".format(end - start),
+                                           "label": label,
+                                           "mean": prediction.mean()})
                 predictions[label].append(prediction)
-        scio.savemat(cfg.inference.output_dir + f'{i}.mat', predictions)
+
+        scio.savemat(cfg.output_dir + f'{i}.mat', predictions)
 
 
 if __name__ == '__main__':
