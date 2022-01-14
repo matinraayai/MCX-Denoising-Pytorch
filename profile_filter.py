d37fcbe6e734e4472bf85f724e35e73e39cc988c
matinraayai <raayaiardani.m@northeastern.edu>
2022-01-14 14:47:05 -0500

    Updated profile script with the new inference function.

--- profile_filter.py
+++ profile_filter.py
@@ -4,6 +4,7 @@ import tqdm
 from config import read_profiling_cfg_file
 import torch
 import torch.autograd.profiler as profiler
+from data.dataset import norm
 
 
 def get_args():
@@ -27,14 +28,22 @@ def main():
     model.train(False)
 
     data_dims = cfg.input_dims
-    x_input = [torch.rand(data_dims) for _ in range(cfg.num_iterations)]
+    x_input = [torch.zeros(data_dims) for _ in range(cfg.num_iterations)]
+    unpaded_volume_slice = eval(cfg.unpaded_volume_slice)
     with profiler.profile(record_shapes=True) as prof:
         for x in tqdm.tqdm(x_input):
             with profiler.record_function("Host to Device Data Transfer"):
                 x = x.cuda()
+            with profiler.record_function("Double copy"):
+                x_lower = norm(x * 10**7)
             with profiler.record_function("Model Inference"):
                 with torch.no_grad():
-                    y = model(x)
+                    lower = model(x_lower)[unpaded_volume_slice]
+                    higher = model(x)[unpaded_volume_slice]
+            with profiler.record_function("Cutoff"):
+                lower = (torch.exp(lower) - 1) / 10**7
+                higher = torch.exp(higher) - 1
+                y = torch.where(x[unpaded_volume_slice] > 0.03, higher, lower).squeeze()
             with profiler.record_function("Device to Host Data Transfer"):
                 y = y.cpu().numpy()
     print(prof.key_averages().table())
