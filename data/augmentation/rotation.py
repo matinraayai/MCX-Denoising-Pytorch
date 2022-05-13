07eb29797c0f6f28ea02d2c09b6d721e21589a3c
matinraayai <raayaiardani.m@northeastern.edu>
2022-05-13 13:04:15 -0400

    Doc update.

--- data/augmentation/rotation.py
+++ data/augmentation/rotation.py
@@ -6,25 +6,21 @@ from torchvision.transforms.functional import rotate
 import torch
 
 
-def select_random_dims(random_state):
+def select_random_plane(random_state):
+    """
+    Selects a random plane using the Numpy random state to be used for applying a random rotation
+    :param random_state: An np.random.RandomState object
+    :return: a tuple of size 2, containing the axis of the randomly selected plane
+    """
     axes = [(1, 2), (1, 3), (2, 3)]
     return axes[random_state.randint(3)]
 
 
 class Rotate(DataAugment):
     """
-    Continuous rotatation of the `xy`-plane.
-
-    If the rotation degree is arbitrary, the sample size for `x`- and `y`-axes should be at
-    least :math:`\sqrt{2}` times larger than the input size to ensure there is no non-valid region
-    after center-crop. This augmentation is applied to both images and masks.
-
-    Args:
-        rot90 (bool): rotate the sample by only 90 degrees. Default: True
-        p (float): probability of applying the augmentation. Default: 0.5
-        additional_targets(dict, optional): additional targets to augment. Default: None
+    Applies a random rotation to a random plane of the input volume
+    :param p: Probability of applying rotation on the image/volume
     """
-
     def __init__(self,
                  p: float = 0.5):
 
@@ -35,8 +31,10 @@ class Rotate(DataAugment):
 
     def __call__(self, sample, random_state=np.random.RandomState()):
         image, label = sample['image'], sample['label']
+        # Since rotate in torchvision only applies its rotation to the first plane, we first have to rotate it by 90
+        # degrees randomly to bring the target plane forward for the rotation
         if len(image.shape) == 4:
-            dims = select_random_dims(random_state)
+            dims = select_random_plane(random_state)
             image, label = torch.rot90(image, 1, dims), torch.rot90(label, 1, dims)
         angle = random_state.rand() * 360.0
         image = rotate(image, angle)
@@ -45,7 +43,8 @@ class Rotate(DataAugment):
             image, label = torch.rot90(image, -1, dims), torch.rot90(label, -1, dims)
             # An independent rotation along a plane
             k = random_state.randint(0, 3)
-            dims = select_random_dims(random_state)
+            dims = select_random_plane(random_state)
+            # Random 90-degree rotation to complete the random 3D rotation
             image, label = torch.rot90(image, k, dims), torch.rot90(label, k, dims)
         sample['image'], sample['label'] = image, label
         return sample
